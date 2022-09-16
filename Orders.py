303893121561260d9af4b24795126520cfbce18c
Assaf <asafaaaad2@gmail.com>
2022-09-16 23:20:45 +0300

    created orders class

---
+++ Orders.py
@@ -0,0 +1,12 @@
+class Orders:
+    def new_order (order):
+        return order
+
+    def update_order (order_name):
+        return order
+
+    def check_for_existing_order (order_name):
+        return True
+
+    def remove_order (order_name):
+        return True
